ae7f1943b890d8f756afe43db475a0cf5d8fa847
Rose <rosexu1996@yahoo.com>
2015-10-24 22:18:00 -0400

    figured out OAuth and got instagram images to show up

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 import os
+import requests
 from flask import Flask
+from flask import render_template, redirect, request
+
 
 app = Flask(__name__)
+
+clientID = "c154c63ac943405e848bf8a936eeb12a"
+clientSecret = "dd9e465214994ed788ecb9781f3c460f"
+grantType = "authorization_code"
+redirectURI = "http://localhost:5000/get-20"
+auth = "https://api.instagram.com/oauth/authorize/?" + "client_id=" + clientID + "&" + "redirect_uri=" +redirectURI + "&response_type=code"
+accessToken = ""
 
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+    return redirect(auth)
+
+
+@app.route('/get-20')
+def getPosts():
+    code = request.args.get('code')
+    result = requests.post("https://api.instagram.com/oauth/access_token", data = {"client_id":clientID,
+                                                                                   "client_secret":clientSecret,
+                                                                                   "grant_type":"authorization_code",
+                                                                                   "redirect_uri":redirectURI,
+                                                                                   "code":code})
+    jsonResult = result.json()
+    global accessToken
+    accessToken = jsonResult['access_token']
+    return render_template("index.html")
+
+
+@app.route('/posts')
+def searchPosts():
+    print accessToken
+    result = requests.get("https://api.instagram.com/v1/tags/capitalone/media/recent?count=20&access_token=" + accessToken)
+    posts = result.json()['data']
+    return render_template("posts.html", posts=posts)
+
+
+if __name__ == '__main__':
+    app.run()
