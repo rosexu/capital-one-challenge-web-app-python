37480f8dccf2321aa8c8aa7bf3bab535e0878acc
Rose <rosexu1996@yahoo.com>
2015-10-30 15:14:58 -0400

    remove debug statements and add some documentation

--- main.py
+++ main.py
@@ -11,53 +11,60 @@ clientID = "c154c63ac943405e848bf8a936eeb12a"
 clientSecret = "dd9e465214994ed788ecb9781f3c460f"
 grantType = "authorization_code"
 redirectURI = "http://localhost:5000/get-20"
-auth = "https://api.instagram.com/oauth/authorize/?" + "client_id=" + clientID + "&" + "redirect_uri=" +redirectURI + "&response_type=code"
+auth = "https://api.instagram.com/oauth/authorize/?" + "client_id=" + clientID + "&" + "redirect_uri=" + redirectURI + "&response_type=code"
 accessToken = ""
 
-
+# When user arrives at the site, redirect him/her to authorize permission to use Instagram
 @app.route('/')
 def hello():
     return redirect(auth)
 
 
+# After user authenticates, stores the access token so we can make
+# api calls and redirect the user to the landing page
 @app.route('/get-20')
-def getPosts():
+def get_access_token():
     code = request.args.get('code')
-    result = requests.post("https://api.instagram.com/oauth/access_token", data = {"client_id":clientID,
-                                                                                   "client_secret":clientSecret,
-                                                                                   "grant_type":"authorization_code",
-                                                                                   "redirect_uri":redirectURI,
-                                                                                   "code":code})
-    jsonResult = result.json()
+    result = requests.post("https://api.instagram.com/oauth/access_token", data={"client_id": clientID,
+                                                                                 "client_secret": clientSecret,
+                                                                                 "grant_type":"authorization_code",
+                                                                                 "redirect_uri": redirectURI,
+                                                                                 "code": code})
+    json_result = result.json()
     global accessToken
-    accessToken = jsonResult['access_token']
+    accessToken = json_result['access_token']
     return render_template("index.html")
 
 
+# return a page of the 20 most recent posts about capital one on instagram
 @app.route('/posts')
 def search_posts():
     posts = get_posts()
     return render_template("posts.html", posts=posts)
 
 
+# Makes a get request to the Instagram api, asking for the 20 most recent
+# posts about Capital One
 def get_posts():
-    print accessToken
     result = requests.get("https://api.instagram.com/v1/tags/capitalone/media/recent?count=20&access_token=" + accessToken)
     return result.json()['data']
 
 
+# return the sentiment analysis summary page
 @app.route('/sentiment')
 def sentiment():
     posts = get_posts()
     list_caps = []
     for post in posts:
-        print(post['caption']['text'])
         list_caps.append(post['caption']['text'])
     sentiment_counts = sentiment_analysis(list_caps)
     return render_template("sentiment.html", sentiment=sentiment_counts)
 
 
-
+# Takes in a list of captions of length 20. Conducts sentiment analysis on
+# them by using the indicoio sentiment analysis API. The api returns a dec
+# number between 0 and 1. The function returns a dictionary of the number of
+# positive, neutral, and negative comments.
 def sentiment_analysis(list_captions):
     indicoio.config.api_key = '64ea7c556b0060d9d7a8e97c8d8968d7'
     list_sentiment = []
@@ -65,16 +72,16 @@ def sentiment_analysis(list_captions):
     num_neu = 0
     num_neg = 0
     for caption in list_captions:
-        sentiment = indicoio.sentiment_hq(caption)
-        list_sentiment.append(sentiment)
+        sentiment1 = indicoio.sentiment_hq(caption)
+        list_sentiment.append(sentiment1)
+        # sentiment is positive
         if sentiment > 0.55:
-            print("positive")
             num_pos += 1
+        # sentiment is neutral
         elif sentiment > 0.45:
-            print("neutral")
             num_neu += 1
+        # sentiment is negative
         else:
-            print("negative")
             num_neg += 1
 
     sentiment_dict = {"positive": num_pos,
@@ -83,9 +90,9 @@ def sentiment_analysis(list_captions):
     return sentiment_dict
 
 
+# returns the user information page
 @app.route('/user/<userid>')
-def getUserInfo(userid=None):
-    print userid
+def get_user_info(userid=None):
     user_info = requests.get("https://api.instagram.com/v1/users/" + userid + "/", params={"access_token": accessToken})
     user = user_info.json()['data']
     return render_template("user.html", user=user)
